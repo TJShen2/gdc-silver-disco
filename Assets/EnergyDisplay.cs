a73185620e06f527ce4fb0b3c1869fd41830b98e
TJShen2 <138247491+TJShen2@users.noreply.github.com>
2024-03-25 15:05:35 -0400

    Update EnergyDisplay.cs

--- Assets/EnergyDisplay.cs
+++ Assets/EnergyDisplay.cs
@@ -21,7 +21,6 @@ public class EnergyDisplay : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        print(playerScript.Energy);
         energyDisplay.SetText("Energy: " + playerScript.Energy + " J");
         energyDisplay.ForceMeshUpdate();
     }
